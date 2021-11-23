8e806b5eee4b9f1511d649e4b05ef921357202a6
andreajiao <andreajiao@gmail.com>
2021-11-23 10:33:39 +0100

    html på project og subproject

---
+++ EksamensprojektVinter2021/src/main/java/com/example/eksamensprojektvinter2021/EksamensprojektVinter2021Application.java
@@ -0,0 +1,13 @@
+package com.example.eksamensprojektvinter2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamensprojektVinter2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamensprojektVinter2021Application.class, args);
+    }
+
+}
