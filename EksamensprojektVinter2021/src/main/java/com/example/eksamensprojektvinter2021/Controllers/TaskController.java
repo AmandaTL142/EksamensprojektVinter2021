070fa01f2ca503f70ad39d9d60c81475f4ba0d58
Casper <casper.aakviststeengaard@gmail.com>
2021-11-23 14:53:37 +0100

    Test controller

--- EksamensprojektVinter2021/src/main/java/com/example/eksamensprojektvinter2021/Controllers/TaskController.java
+++ EksamensprojektVinter2021/src/main/java/com/example/eksamensprojektvinter2021/Controllers/TaskController.java
@@ -1,4 +1,13 @@
 package com.example.eksamensprojektvinter2021.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TaskController {
+
+    @GetMapping("/")
+    public String index (){
+        return "project";
+    }
 }
