18bde2bf9dae24dbb32eda51fd3fd56e4eae201f
Christian Hundahl <chri02jx@stud.kea.dk>
2021-11-25 13:33:49 +0100

    Log-in WIP

--- EksamensprojektVinter2021/src/main/java/com/example/eksamensprojektvinter2021/Controllers/LoginController.java
+++ EksamensprojektVinter2021/src/main/java/com/example/eksamensprojektvinter2021/Controllers/LoginController.java
@@ -1,6 +1,39 @@
 package com.example.eksamensprojektvinter2021.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
+
 public class LoginController {
     //Her laver vi noget log-in logik
     //Benyt sessions
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(WebRequest wr, HttpSession session) throws SQLException {
+        String name = wr.getParameter("name");
+        String password = wr.getParameter("password");
+
+        //Evaluer hvis login matcher database
+        boolean validPass = LogingService.login(id, password);
+
+        //Sæt en bruger som enten Manager eller Medarbejder allerede ved login
+        if (validPass) {
+            int userId = LoginService.findUserId(name, password);
+            session.setAttribute("id", userId);
+            //if (isManager.userId) {
+            //session.setAttribute ("manager", true)
+            //}
+            return "redirect:/show-projects";
+        }
+        else {
+            return "/login";
+        }
+    }
 }
